b9279ee948319ce2cd299eac1ded02549780327a
Proofs.io <proofs@proofs.io>
2025-06-07 15:49:31 +0000

    Updated code.js to initialize the plugin UI using figma.showUI and setup basic message passing between the main plugin script and the UI iframe.

--- figma-variables-plugin/code.js
+++ figma-variables-plugin/code.js
@@ -1,2 +1,15 @@
-/* code.js - Main plugin script placeholder */
-console.log('Figma Variables Plugin main script loaded.');
+/* code.js - Main plugin script for Figma Variables Plugin */
+
+figma.showUI(__html__, { width: 320, height: 600 });
+
+figma.ui.onmessage = (msg) => {
+  if (msg.type === 'alert') {
+    figma.notify(msg.message);
+  }
+  // Additional message handlers can be added here
+};
+
+// Optionally, send an initial message to the UI
+figma.ui.postMessage({ type: 'initialize', variables: [] });
+
+console.log('Figma Variables Plugin main script loaded and UI shown.');
